e4880cf11036bdd2192e9f7a271d007759e0584c
piratesedge <andreamaspero@gmail.com>
2023-10-24 11:42:33 +1100

    Update Quotes
    Removed strange quotes and placed normal ""

--- Demotest.ps1
+++ Demotest.ps1
@@ -1,20 +1,10 @@
-Write-Host -ForegroundColor Green “Starting Zero Touch Setup”
+Write-Host -ForegroundColor Green "Starting Zero Touch Setup"
 
 Start-Sleep -Seconds 1
-Write-Host -ForegroundColor Green “Starting AFCA OSDCloud Setup”
+Write-Host -ForegroundColor Green "Starting AFCA OSDCloud Setup"
 
-#Make sure I have the latest OSD Content
-Write-Host -ForegroundColor Green “Updating OSD PowerShell Module”
 
-Install-Module OSD -Force
-
-Write-Host -ForegroundColor Green “Importing OSD PowerShell Module”
-
-Import-Module OSD -Force
-
-#Start OSDCloud ZTI the RIGHT way
-
-Write-Host -ForegroundColor Green “Start OSDCloud”
+Write-Host -ForegroundColor Green "Starting Automated OS Installation Process"
 
 Start-OSDCloud -OSName 'D:\OSDCloud\OS\Win11_22H2_Enterprise.wim' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -ZTI
 
