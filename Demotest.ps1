a8150b21003f3ea508f5f8bf66012d5bc4f284fc
piratesedge <andreamaspero@gmail.com>
2023-10-24 12:13:09 +1100

    Update Demotest.ps1

--- Demotest.ps1
+++ Demotest.ps1
@@ -6,7 +6,7 @@ Write-Host -ForegroundColor Green "Starting AFCA OSDCloud Setup"
 
 Write-Host -ForegroundColor Green "Starting Automated OS Installation Process"
 
-Start-OSDCloud  -FindImageFile -OSName 'Win11_22H2_Enterprise.wim' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -ZTI
+Start-OSDCloud  -FindImageFile -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -ZTI
 
 #Restart from WinPE
 
