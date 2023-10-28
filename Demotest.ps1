61a688f112ecf466743122827c0a6a5eeb8df6fd
piratesedge <andreamaspero@gmail.com>
2023-10-28 14:18:24 +1100

    Demotest.ps1

--- Demotest.ps1
+++ Demotest.ps1
@@ -6,12 +6,14 @@ Write-Host -ForegroundColor Green "Starting AFCA OSDCloud Setup"
 
 Write-Host -ForegroundColor Green "Starting Automated OS Installation Process"
 
-Start-OSDCloud -ImageFileURL "D:\OSDCloud\0S\Win11_22H2_Enterprise.wim" -OSImageIndex "1" -ZTI
+#Start-OSDCloud -ImageFileURL "D:\OSDCloud\0S\Win11_22H2_Enterprise.wim" -OSImageIndex "1" -ZTI
 
-#Start-OSDCloud  -FindImageFile -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -ZTI
+Start-OSDCloud  -FindImageFile -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -ZTI
 
 #Restart from WinPE
 
 Write-Host -ForegroundColor Green "Restarting in 3 seconds!"
 
-Start-Sleep -Seconds 15
+Start-Sleep -Seconds 3
+
+wpeutil reboot
