a2a25c88830cf8711924d5d8bd79471cf7a52f6a
piratesedge <andreamaspero@gmail.com>
2023-11-15 19:16:33 +1100

    Demotest.ps1

--- Demotest.ps1
+++ Demotest.ps1
@@ -1,14 +1,64 @@
-Write-Host -ForegroundColor Green "Starting Zero Touch Setup"
+#================================================
+#   [OSDCloud] Update Module
+#================================================
 
-Start-Sleep -Seconds 1
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+
+Import-Module OSD -Force
+
 Write-Host -ForegroundColor Green "Starting AFCA OSDCloud Setup"
 
+Start-Sleep -Seconds 1
 
 Write-Host -ForegroundColor Green "Starting Automated OS Installation Process"
 
-#Start-OSDCloud -ImageFileURL "D:\OSDCloud\0S\Win11_22H2_Enterprise.wim" -OSImageIndex "1" -ZTI
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "22H2"
+    OSEdition = "Enterprise"
+    OSLanguage = "en-us"
+    OSLicense = "Volume"
+    ZTI = $true
+    Firmware = $false
+}
+Start-OSDCloud @Params
 
-Start-OSDCloud -FindImageFile -OSImageIndex "3" -ZTI
+function Copy-FromBootImage {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $FileName
+    )
+    process {
+        $SourceFilePath = Join-Path -Path $env:SystemDrive -ChildPath ("OSDCloud\Scripts\" + $FileName)
+        $DestinationFolderPath = "C:\temp"
+        if (-not $env:SystemDrive) {
+            Write-Error "This script must be run in a WinPE environment."
+            return
+        }
+        try {
+            if (Test-Path -Path $SourceFilePath) {
+                if (-not (Test-Path -Path $DestinationFolderPath)) {
+                    New-Item -ItemType Directory -Path $DestinationFolderPath | Out-Null
+                }
+                $fullDestinationPath = Join-Path -Path $DestinationFolderPath -ChildPath $FileName
+                Copy-Item -Path $SourceFilePath -Destination $fullDestinationPath -Force -ErrorAction Stop
+                Write-Output "File '$SourceFilePath' has been copied to '$fullDestinationPath'"
+            } else {
+                throw "Source file '$SourceFilePath' does not exist."
+            }
+        }
+        catch {
+            Write-Error $_.Exception.Message
+        }
+    }
+}
 
 function Create-ScriptFolder {
     param (
@@ -19,50 +69,38 @@ function Create-ScriptFolder {
     }
 }
 
-function Create-ScheduledTask {
-    param (
-        [string]$TaskName,
-        [string]$TaskDescription,
-        [string]$TriggerDelay,
-        [string]$ActionPath,
-        [string]$ActionArguments
-    )
-    $ShedService = New-Object -comobject 'Schedule.Service'
-    $ShedService.Connect()
-
-    $Task = $ShedService.NewTask(0)
-    $Task.RegistrationInfo.Description = $TaskDescription
-    $Task.Settings.Enabled = $true
-    $Task.Settings.AllowDemandStart = $true
-
-    $trigger = $Task.Triggers.Create(9)
-    $trigger.Delay = $TriggerDelay
-    $trigger.Enabled = $true
-
-    $action = $Task.Actions.Create(0)
-    $action.Path = $ActionPath
-    $action.Arguments = $ActionArguments
-
-    $taskFolder = $ShedService.GetFolder("\")
-    $taskFolder.RegisterTaskDefinition($TaskName, $Task, 6, "SYSTEM", $NULL, 5)
-}
-
 # Set script folder paths
-$scriptFolderPath = "$env:SystemDrive\OSDCloud\Scripts"
+$scriptFolderPath = "C:\temp\"
 
 # Create script folder
-Create-ScriptFolder -FolderPath $scriptFolderPath
+Create-Folder -FolderPath $scriptFolderPath
 
-# Example of creating a scheduled task
-Create-ScheduledTask -TaskName "Start OOBE AFCA Agent" -TaskDescription "This task is used to auto start the OOBE Agent to automate user driven provisioning" -TriggerDelay 'PT60S' -ActionPath "$scriptFolderPath\OOBE-Task.exe" -ActionArguments '-SomeArguments'
+#Assign PC to User
+Start-Process "$env:SystemDrive\OSDCloud\Scripts\OSDCloud-Assign-User.exe" -ArgumentList "ArgumentsForExecutable" -Wait
 
-#Copy .exe to file
+# Download ServiceUI.exe
+Write-Host -ForegroundColor Gray "Download ServiceUI.exe from GitHub Repo"
+Invoke-WebRequest https://github.com/piratesedge/intune/raw/main/ServiceUI64.exe -OutFile "C:\temp\ServiceUI.exe"
 
+#Copy Files from Image to C: Drive
+Copy-FromBootImage -FileName "OOBE-Task.exe"
+Copy-FromBootImage -FileName "SpecialiseTaskScheduler.ps1"
+Copy-FromBootImage -FileName "OOBE-Startup-Script.ps1"
+Copy-FromBootImage -FileName "SendKeysSHIFTnF10.ps1"
 
-#Restart from WinPE
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
+powershell.exe -Command Start-Process "C:\temp\SpecialiseTaskScheduler.ps1
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
-Write-Host -ForegroundColor Green "Restarting in 3 seconds!"
-
-Start-Sleep -Seconds 3
-
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 5 seconds!"
+Start-Sleep -Seconds 5
 wpeutil reboot
