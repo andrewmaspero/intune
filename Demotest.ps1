72d48ca25add2e642e61cb9e4afdf34f3360917c
piratesedge <andreamaspero@gmail.com>
2023-11-15 13:29:17 +1100

    Demotest.ps1

--- Demotest.ps1
+++ Demotest.ps1
@@ -8,7 +8,56 @@ Write-Host -ForegroundColor Green "Starting Automated OS Installation Process"
 
 #Start-OSDCloud -ImageFileURL "D:\OSDCloud\0S\Win11_22H2_Enterprise.wim" -OSImageIndex "1" -ZTI
 
-Start-OSDCloud -FindImageFile -OSImageIndex "1" -ZTI
+Start-OSDCloud -FindImageFile -OSImageIndex "3" -ZTI
+
+function Create-ScriptFolder {
+    param (
+        [string]$FolderPath
+    )
+    If (!(Test-Path -Path $FolderPath)) {
+        New-Item -Path $FolderPath -ItemType Directory -Force | Out-Null
+    }
+}
+
+function Create-ScheduledTask {
+    param (
+        [string]$TaskName,
+        [string]$TaskDescription,
+        [string]$TriggerDelay,
+        [string]$ActionPath,
+        [string]$ActionArguments
+    )
+    $ShedService = New-Object -comobject 'Schedule.Service'
+    $ShedService.Connect()
+
+    $Task = $ShedService.NewTask(0)
+    $Task.RegistrationInfo.Description = $TaskDescription
+    $Task.Settings.Enabled = $true
+    $Task.Settings.AllowDemandStart = $true
+
+    $trigger = $Task.Triggers.Create(9)
+    $trigger.Delay = $TriggerDelay
+    $trigger.Enabled = $true
+
+    $action = $Task.Actions.Create(0)
+    $action.Path = $ActionPath
+    $action.Arguments = $ActionArguments
+
+    $taskFolder = $ShedService.GetFolder("\")
+    $taskFolder.RegisterTaskDefinition($TaskName, $Task, 6, "SYSTEM", $NULL, 5)
+}
+
+# Set script folder paths
+$scriptFolderPath = "$env:SystemDrive\OSDCloud\Scripts"
+
+# Create script folder
+Create-ScriptFolder -FolderPath $scriptFolderPath
+
+# Example of creating a scheduled task
+Create-ScheduledTask -TaskName "Start OOBE AFCA Agent" -TaskDescription "This task is used to auto start the OOBE Agent to automate user driven provisioning" -TriggerDelay 'PT60S' -ActionPath "$scriptFolderPath\OOBE-Task.exe" -ActionArguments '-SomeArguments'
+
+#Copy .exe to file
+
 
 #Restart from WinPE
 
