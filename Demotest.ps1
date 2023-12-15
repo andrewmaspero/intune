a4c4ce2f35b9fdf33ee48e0aa56242302df4499f
Andrew M <andreamaspero@gmail.com>
2023-12-15 13:54:04 +1100

    Demotest.ps1

--- Demotest.ps1
+++ Demotest.ps1
@@ -171,7 +171,7 @@ function Copy-FromBootImage {
         [string] $FileName
     )
     process {
-        $SourceFilePath = Join-Path -Path "D:\OSDCloud\Scripts" -ChildPath $FileName
+        $SourceFilePath = Join-Path -Path "E:\OSDCloud\Scripts" -ChildPath $FileName
         $DestinationFolderPath = "C:\temp"
         if (-not $env:SystemDrive) {
             Write-Error "This script must be run in a WinPE environment."
@@ -303,7 +303,7 @@ function Create-Folder {
 Create-Folder -FolderPath "C:\temp"
 
 #Assign PC to User
-Start-Process "D:\OSDCloud\Scripts\OSDCloud-Assign-User.exe" -ArgumentList "ArgumentsForExecutable" -Wait
+Start-Process "E:\OSDCloud\Scripts\OSDCloud-Assign-User.exe" -ArgumentList "ArgumentsForExecutable" -Wait
 Start-Sleep -Seconds 1
 
 #Copy Files from Image to C: Drive
