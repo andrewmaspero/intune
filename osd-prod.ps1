4b888ed26e5d25d4b34cc9ccb090d707c41d10e0
Andrew M <andreamaspero@gmail.com>
2024-08-19 15:40:53 +1000

    Update osd-prod.ps1

--- osd-prod.ps1
+++ osd-prod.ps1
@@ -176,10 +176,12 @@ Write-Host -ForegroundColor Green "Starting Automated OS Installation Process"
 #   [OS] Params and Start-OSDCloud
 #=======================================================================
 $Params = @{
-    $ImageFileUrl = "https://autopro.afca.org.au/hosted-files/Windows11_23H2_22621_3958_WITH_ALLDRIVERS_WIM.wim"
-    $ImageIndex = 1
+    ImageFileUrl = "https://autopro.afca.org.au/hosted-files/Windows11_23H2_22621_3958_WITH_ALLDRIVERS_WIM.wim"
+    ImageIndex = 1
     OSLanguage = "en-us"
+    OSLicense = "Volume"
     ZTI = $true
+    Firmware = $false
 }
 
 Start-OSDCloud @Params
